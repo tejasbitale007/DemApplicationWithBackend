bd822bbc0eed75f2728ef34ecadbf45af302e8b6
Tejas Bitale <92600946+tejasbitale007@users.noreply.github.com>
2024-03-25 08:42:57 +0530

    Added folderStructure inside DemoApplication

---
+++ components/BottomStackNavigator.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const BottomStackNavigator = () => {
+  return (
+    <View>
+      <Text>BottomStackNavigator</Text>
+    </View>
+  )
+}
+
+export default BottomStackNavigator
